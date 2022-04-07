8c79e4bf23d13367c6b3df389191e4c239885f5c
Read Langworthy <rlangworthy@gmail.com>
2022-04-07 15:41:22 -0700

    commit tree b792834e0ff3470b00e995c91d82c869d288eaee

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,27 @@
 class StudentsController < ApplicationController
 
+  # def index
+  #   students = Student.all
+  #   render json: students
+  # end
+
   def index
-    students = Student.all
-    render json: students
+    student_first = Student.find_by(first_name: params["name"].capitalize )
+    student_last = Student.find_by(last_name: params["name"].capitalize )
+    array = []
+    if student_first
+      array << student_first
+    end
+    if student_last
+      array << student_last
+    end
+
+    render json: array
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
